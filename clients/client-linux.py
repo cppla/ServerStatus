df97a54b9cd2fd94541116dba19ea0d9080a2b9f
ubuntu <ubuntu@ubuntu.com>
2018-09-03 12:22:13 +0800

    update

--- clients/client-linux.py
+++ clients/client-linux.py
@@ -151,10 +151,10 @@ def get_network(ip_version):
         HOST = "ipv6.google.com"
     try:
         s = socket.create_connection((HOST, 80), 2)
+        s.close()
         return True
     except:
-        pass
-    return False
+        return False
 
 lostRate = {
     '10010': 0.0,
