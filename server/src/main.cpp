c0ea66171a3fca4cdf3a0e403b68b900b06ec1b2
windows11 <windows11@pc>
2021-11-02 16:35:42 +0800

    reduce month traffic diff

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -264,10 +264,10 @@ void CMain::JSONUpdateThread(void *pUser)
 				else
 					str_format(aUptime, sizeof(aUptime), "%02d:%02d:%02d", (int)(pClients[i].m_Stats.m_Uptime/60.0/60.0), (int)((pClients[i].m_Stats.m_Uptime/60)%60), (int)((pClients[i].m_Stats.m_Uptime)%60));
 
-				// track month network traffic, diff: 2021-10-01 00:10
+				// track month network traffic, diff: 2021-10-01 00:05, 5minutes
 				// last_network_in/out is last record flag.
                 time_t currentStamp = (long long)time(/*ago*/0);
-                if(0 == pClients[i].m_LastNetworkIN || (localtime(&currentStamp)->tm_mday == pClients[i].m_aMonthStart && localtime(&currentStamp)->tm_hour == 0 && localtime(&currentStamp)->tm_min < 10))
+                if(0 == pClients[i].m_LastNetworkIN || (localtime(&currentStamp)->tm_mday == pClients[i].m_aMonthStart && localtime(&currentStamp)->tm_hour == 0 && localtime(&currentStamp)->tm_min < 5))
                 {
                     pClients[i].m_LastNetworkIN = pClients[i].m_Stats.m_NetworkIN;
                     pClients[i].m_LastNetworkOUT = pClients[i].m_Stats.m_NetworkOUT;
