4e175d0a81ed869fd2606b105111727251f336f6
oc <oc@oc>
2022-01-07 18:45:17 +0800

    change max client 128 to 256

--- server/src/network.h
+++ server/src/network.h
@@ -10,7 +10,7 @@ enum
 	NET_CONNSTATE_ERROR=4,
 
 	NET_MAX_PACKETSIZE = 1400,
-	NET_MAX_CLIENTS = 128
+	NET_MAX_CLIENTS = 256
 };
 
 typedef int (*NETFUNC_DELCLIENT)(int ClientID, const char* pReason, void *pUser);
